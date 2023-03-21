b9288bbbc8ae9f0f1c710f7f3fb7c6daed167cc1
MdMizanurRahman1 <mizan.rahman2217@gmail.com>
2023-03-21 16:00:27 +0200

    usestate practiced

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
 
+
+
+
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Counter></Counter> */}
+      <ExternalUsers></ExternalUsers>
     </div>
   );
 }
+
+
+function ExternalUsers() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, []);
+  return (
+    <div>
+      <h2>External Users</h2>
+      <p>{users.length}</p>
+      {
+        users.map(user => <User name={user.name} email={user.email}></User>)
+      }
+    </div>
+  )
+}
+
+
+function User(props) {
+  return (
+    <div style={{ border: '2px solid green', margin: '10px' }}>
+      <h3>Name: {props.name}</h3>
+      <p>Email: {props.email}</p>
+    </div>
+  )
+}
+
+
+function Counter() {
+  const [count, setCount] = useState(35);
+
+  const increaseCount = () => setCount(count + 1);
+  const decreaseCount = () => setCount(count - 1);
+
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={increaseCount}>Increase</button>
+      <button onClick={decreaseCount}>Decrease</button>
+    </div>
+  )
+}
+
+
+
+// const nayoks = ['jasim', 'bappa', 'habib', 'jabib', 'labib', 'tuhin']
+// {
+//   nayoks.map(nayok => <li> Name: {nayok}</li>)
+// }
+
+// {
+//   nayoks.map(nayok => <Person name={nayok}></Person>)
+// }
+
+// {/* <Person nayok="Mizan" nayika="Mohsina"></Person>
+// <Person nayok="Bappa" nayika="Seka"></Person>
+// <Person nayok="Alamgir" nayika="Sabina"></Person> */}
+// <Friend movie="Janina" rate="5.6"></Friend>
+// <Friend movie="Fazil" rate="0.56"></Friend>
+
+// function Person(props) {
+//   return (
+//     <div className='person'>
+//       <h1>{props.name}</h1>
+//       <p>{props.nayika}</p>
+//     </div>
+//   )
+// }
+
+// function Friend(props) {
+//   return (
+//     <div className='container'>
+//       <h1>Name: {props.movie}</h1>
+//       <p>Rattings: {props.rate}</p>
+//     </div>
+//   )
+// }
 
 export default App;
